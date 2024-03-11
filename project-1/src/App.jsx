1e35bfcfa414f3df1ae40f807f77af3d79b8d351
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-12 00:00:02 +0530

    app.jsx updated

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
 
   return (
     <>
       <Home/>
+      <Layout/>
     </>
   )
 }
