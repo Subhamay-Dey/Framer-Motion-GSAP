366f558985161e4f8b163d5f18d64d64141c1a7e
Subhamay-Dey <ncdey1966@gmail.com>
2025-03-15 02:36:23 +0530

    Text Revealing Animation done - Added GSAP for text animation and implemented Textsplit component in Home page ✅

--- project-5/src/app/page.tsx
+++ project-5/src/app/page.tsx
@@ -1,11 +1,10 @@
+import Textsplit from "@/components/Textsplit";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <section>
-        <h1 className="text-7xl font-extrabold text-center mt-10">Welcome</h1>
-      </section>
+      <Textsplit/>
     </>
   );
 }
--- project-5/src/components/Textsplit.tsx
+++ project-5/src/components/Textsplit.tsx
@@ -1,15 +1,66 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useRef } from 'react';
+import styles from "./text.module.scss";
+import {gsap} from "gsap"
+import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+
+function useArrayRef<T>() {
+    const refs = useRef<T[]>([]);
+
+    const setRef = (ref: T | null) => {
+        if (ref && !refs.current.includes(ref)) {
+            refs.current.push(ref);
+        }
+    };
+
+    return [refs, setRef] as const;
+}
 
 function Textsplit() {
-    const text = "the main body of a book or other piece of writing, as distinct from other material such as notes, appendices, and illustrations."
+
+    const [lettersRef, setLettersRef] = useArrayRef();
+
+    const triggerRef = useRef(null)
+
+    gsap.registerPlugin(ScrollTrigger)
+
+    useEffect(() => {
+        if (lettersRef.current.length === 0) return;
+        const reveal = gsap.to(lettersRef.current,
+            {
+                scrollTrigger: {
+                    trigger: triggerRef.current,
+                    scrub: true,
+                    start: "top 80%",
+                    end: "bottom center",
+                },
+                color: "white",
+                duration: 8,
+                stagger: 1,
+            }
+        )
+
+        return () => {
+            reveal.kill();
+        }
+    }, [lettersRef])
+    
+
+    const text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout that a reader will be distracted by the readable content."
+
   return (
-    <div className='reveal'>
-        <div>
-            {text.split("").map((letter, index) => (
-                <span className='reveal-text' key={index}>{letter}</span>
-            ))}
-        </div>
-    </div>
+    <>
+        <div className='h-[100vh]'></div>
+            <div className={styles.reveal}>
+                <div ref={triggerRef}>
+                    {text.split("").map((letter, index) => (
+                        <span className={styles.reveal_text} key={index} ref={setLettersRef}>{letter}</span>
+                    ))}
+                </div>
+            </div>
+        <div className='h-[100vh]'></div>
+    </>
   )
 }
 
